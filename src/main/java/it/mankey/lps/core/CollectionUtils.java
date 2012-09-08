60720b6302530c557970ae9aec8e15709920e1cd
Alessandro Mecca <alessandro.mecca@gmail.com>
2012-09-08 21:33:59 +0100

    CollectionUtils: added map(Iterator, Mapper)

--- src/main/java/it/mankey/lps/core/CollectionUtils.java
+++ src/main/java/it/mankey/lps/core/CollectionUtils.java
@@ -1,6 +1,7 @@
 package it.mankey.lps.core;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -11,6 +12,14 @@ public class CollectionUtils {
         final List<T> result = new ArrayList<T>(list.size());
         for (final E element : list) {
             result.add(mapper.map(element));
+        }
+        return result;
+    }
+
+    public static <E, T> List<T> map(final Iterator<E> list, final Mapper<E, T> mapper) {
+        final List<T> result = new ArrayList<T>();
+        while (list.hasNext()) {
+            result.add(mapper.map(list.next()));
         }
         return result;
     }
